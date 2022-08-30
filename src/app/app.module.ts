9a37100269fa0ccb77ce0e1741dd50186179d807
STAGIAIRE IT <stagiaire-it@local.rawsur.com>
2022-08-30 09:49:18 +0100

    config pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -6,9 +8,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HotelListComponent } from './hotel-list/hotel-list.component';
 
+registerLocaleData(localeFr, 'fr');
+
 @NgModule({
   declarations: [AppComponent, HotelListComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -1,6 +1,5 @@
-import { Person } from './../models/Person';
+
 import { Component, OnInit } from '@angular/core';
-import * as xmlJs from 'xml-js';
 
 @Component({
   selector: 'app-hotel-list',
@@ -15,57 +14,17 @@ export class HotelListComponent implements OnInit
   public showBadge: boolean = true;
   public hotelFilter = 'mot';
 
-  public person: Person = new Person();
-
 
   constructor() { }
 
   ngOnInit(): void
   {
     this.hotels = [
-      {
-        "hotelId": 1,
-        "hotelName": "Buea sweet life",
-        "description": "Belle vue au bord de la mer",
-        "price": 230.5,
-        "imageUrl": "assets/img/hotel-room.jpg",
-        "rating": 3.5
-      },
-      {
-        "hotelId": 2,
-        "hotelName": "Marakech",
-        "description": "Profitez de la vue sur les montagnes",
-        "price": 145.5,
-        "imageUrl": "assets/img/the-interior.jpg",
-        "rating": 5
-      },
-      {
-        "hotelId": 3,
-        "hotelName": "Abudja new look palace",
-        "description": "Séjour complet avec service de voitures",
-        "price": 120.12,
-        "imageUrl": "assets/img/indoors.jpg",
-        "rating": 4
-      },
-      {
-        "hotelId": 4,
-        "hotelName": "Cape town city",
-        "description": "Magnifique cadre pour votre séjour",
-        "price": 135.12,
-        "imageUrl": "assets/img/window.jpg",
-        "rating": 2.5
-      }
+      { "hotelId": 1, "hotelName": "Buea sweet life", "description": "Belle vue au bord de la mer", "price": 230.5, "imageUrl": "assets/img/hotel-room.jpg", "rating": 3.5 },
+      { "hotelId": 2, "hotelName": "Marakech", "description": "Profitez de la vue sur les montagnes", "price": 145.5, "imageUrl": "assets/img/the-interior.jpg", "rating": 5 },
+      { "hotelId": 3, "hotelName": "Abudja new look palace", "description": "Séjour complet avec service de voitures", "price": 120.12, "imageUrl": "assets/img/indoors.jpg", "rating": 4 },
+      { "hotelId": 4, "hotelName": "Cape town city", "description": "Magnifique cadre pour votre séjour", "price": 135.12, "imageUrl": "assets/img/window.jpg", "rating": 2.5 }
     ];
-
-    console.log('====================================');
-    console.log(this.getWithXmlJsLibrary(this.person));
-    console.log('====================================');
-  }
-
-  getWithXmlJsLibrary(obj: Person)
-  {
-    const options = { compact: true, ignoreComment: true, spaces: 4 };
-    return xmlJs.js2xml(obj, options);     // js2xml
   }
 
   public toggleIsNewBadge(): void
